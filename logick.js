837535e7cc3b566405b55d7529c024c1e293972d
asifislam072 <asifislam47348@gmail.com>
2024-02-19 02:29:50 +0600

    ticket

---
+++ logick.js
@@ -0,0 +1,6 @@
+function bayTicket(){
+    const scrollIntoView = document.getElementById('see-all-offer')
+    if(scrollIntoView){
+        scrollIntoView.scrollIntoView({behavior :'smooth'})
+    }
+}
